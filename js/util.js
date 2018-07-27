2601192dcc5a32d66db332af9049acbe5df7becc
单是昊 <949589855@qq.com>
2018-07-27 11:44:32 +0800

    注释

--- js/util.js
+++ js/util.js
@@ -14,6 +14,9 @@ function initTreeJson(treeJson, idKey, pidKey, childKey) {
 	}
 	for(var i = 0; i < treeJson.length; i++) {
 		var cur = treeJson[i]
+		// 这里的意思是normalizedId返回的为0或者null
+		// 说明这一级是最外层id 把最外层push进ret，
+		// 然后对最外层节点cur进行递归
 		if(!normalizedId(cur[pidKey])) {
 			ret.push(cur)
 			initChild(cur)
